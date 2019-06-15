8262b044f518fd63e3f6698186a744e5c133c9cf
Kishita-Gupta <51318750+Kishita-Gupta@users.noreply.github.com>
2019-06-15 15:58:54 +0530

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+j=[i for i in adhoc if i>5]
+print("Numbers which are greater than 5:",j)
+k=[i for i in adhoc if i<=2]
+print("Numbers which are equals to less than 2:",k)
+
